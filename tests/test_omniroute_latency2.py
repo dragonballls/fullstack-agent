a824ae876f4558104c342bd6b22fd4f66d611da8
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-15 14:47:16 -0700

    chore: remove duplicate latency test scaffold

--- tests/test_omniroute_latency2.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class OmniRouteLatencyPlaceholder(unittest.TestCase):
-    def test_latency_health_api_will_be_added(self):
-        self.skipTest("implementation test placeholder")
