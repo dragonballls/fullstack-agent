d629c3d82c9a61cb667eafe6704eace31c72616c
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-15 14:43:31 -0700

    test: add temporary latency optimization test scaffold

---
+++ tests/test_omniroute_latency2.py
@@ -0,0 +1,5 @@
+import unittest
+
+class OmniRouteLatencyPlaceholder(unittest.TestCase):
+    def test_latency_health_api_will_be_added(self):
+        self.skipTest("implementation test placeholder")
