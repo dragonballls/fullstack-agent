0e7d2c2c98a310dc8a7a69b45824ebb9f822d0fc
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-15 16:34:15 -0700

    test: inject desktop adapter for lifecycle tests

--- tests/test_hand_control_runtime.py
+++ tests/test_hand_control_runtime.py
@@ -1,8 +1,29 @@
 from quality_of_life.hand_control_runtime import HandControlRuntime
 
 
+class FakePyAutoGUI:
+    def mouseUp(self):
+        return None
+
+    def size(self):
+        return (1920, 1080)
+
+
+class FakeController:
+    pyautogui = FakePyAutoGUI()
+
+    def move(self, _x, _y):
+        return None
+
+    def click(self, button="left", clicks=1):
+        return None
+
+    def scroll(self, _amount):
+        return None
+
+
 def test_hand_control_runtime_starts_disabled_and_is_idempotent():
-    runtime = HandControlRuntime()
+    runtime = HandControlRuntime(controller=FakeController())
     assert runtime.enabled is False
     assert runtime.start() is True
     assert runtime.enabled is True
@@ -12,6 +33,6 @@ def test_hand_control_runtime_starts_disabled_and_is_idempotent():
 
 
 def test_hand_control_runtime_reports_local_tracker_url():
-    runtime = HandControlRuntime()
+    runtime = HandControlRuntime(controller=FakeController())
     assert runtime.url == "http://127.0.0.1:8795/"
     runtime.stop()
