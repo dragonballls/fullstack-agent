188bd021401e0ce4fecff8e639746d674b7ab763
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-15 17:08:36 -0700

    feat: export account service interfaces

--- quality_of_life/__init__.py
+++ quality_of_life/__init__.py
@@ -1,5 +1,7 @@
 """Optional quality-of-life capability layer for fullstack-agent."""
 
+from .account_integrations import AccountIdentity, AccountSelector, AuthorizationState, ServiceProvider
+from .account_manager import AccountServiceManager
 from .agent_orchestrator import AgentOrchestrator, OrchestrationEvent, OrchestrationResult
 from .capabilities import OPERATION_CATALOG, OperationRisk, OperationSpec, operation, operations_for
 from .computer_use import ComputerUseAction, ComputerUseAgent, ComputerUseResult, RouterComputerUsePlanner, ScreenObserver
@@ -13,8 +15,13 @@ from .permissions import Capability, CapabilityDenied, CapabilityPolicy
 from .readiness import CheckStatus, ReadinessCheck, ReadinessReport, check_readiness, format_report
 from .router import CloudModelRouter, ProviderResult, ProviderTarget
 from .runtime import JarvisRuntime
+from .service_adapters import SERVICE_OPERATIONS, ServiceOperation, ServiceResult, service_operation
 
 __all__ = [
+    "AccountIdentity",
+    "AccountSelector",
+    "AccountServiceManager",
+    "AuthorizationState",
     "Action",
     "AgentOrchestrator",
     "Capability",
@@ -47,7 +54,11 @@ __all__ = [
     "ReadinessReport",
     "RequestProfile",
     "RouterComputerUsePlanner",
+    "SERVICE_OPERATIONS",
     "ScreenObserver",
+    "ServiceOperation",
+    "ServiceProvider",
+    "ServiceResult",
     "SpecialistTask",
     "ToolRegistry",
     "ToolSpec",
@@ -59,4 +70,5 @@ __all__ = [
     "operation",
     "operations_for",
     "parse_intent",
+    "service_operation",
 ]
